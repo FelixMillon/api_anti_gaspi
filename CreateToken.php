0de15053869bbb3f2a736954ee2d736f0fbfb28b
FelixMillon <felix.millon@outlook.com>
2023-05-21 18:14:51 +0200

    createtokenredis

--- CreateToken.php
+++ CreateToken.php
@@ -3,6 +3,7 @@ require_once ("controleur/controleur.class.php");
 
 if (isset($_REQUEST['email']))
 {
+    Controleur::connexion($host, $bdd, $user, $mdp);
     print( Controleur::generateToken ($_REQUEST['email']));
 }else{
     print("error no email found");
